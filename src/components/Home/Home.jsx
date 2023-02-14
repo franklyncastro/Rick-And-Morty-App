7e380b4c15525c0dba899ef8e01913988e03fb55
franklyncastro <franklyncastro@hotmail.com>
2023-02-14 02:05:26 -0400

    update styles 2.0

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import Card from './Card'
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import style from './SearchBar.module.css';
+import React from "react";
+import style from "./SearchBar.module.css";
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function SearchBar({ onSearch, random }) {
-
   const [character, setCharacter] = useState(0);
 
   const handleSearch = (event) => {
@@ -14,19 +13,26 @@ export default function SearchBar({ onSearch, random }) {
 
   return (
     <div className={style.container}>
-      <input type="search" className={style.input} placeholder='Buscar'onChange={handleSearch}/>
-      <button onClick={()=> onSearch(character)} className={style.btn}><span>
-      <Link to='/home' className={style.link}>
-        Agregar
-        </Link>
-        </span></button>
-      
-      <button className={style.btn} onClick={random}>
-        <Link to='/home' className={style.link}>
-        Aleatorio
-        </Link>
-      </button>
-      
+      <input
+        type="search"
+        className={style.input}
+        placeholder="Buscar"
+        onChange={handleSearch}
+      />
+      <div className={style.containerBtn}>
+        <button onClick={() => onSearch(character)} className={style.btn}>
+          <span>
+            <Link to="/home" className={style.link}>
+              Agregar
+            </Link>
+          </span>
+        </button>
+        <button className={style.btn} onClick={random}>
+          <Link to="/home" className={style.link}>
+            Aleatorio
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
