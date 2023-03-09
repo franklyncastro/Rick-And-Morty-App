acfaa444892e99188a12f25467651a34f1038408
Franklyn Castro Dev <fcastro@ljv02helphd02.om.do>
2023-03-09 11:31:24 -0400

    modificaciones en general

--- front/src/components/Favorite/Favorite.jsx
+++ front/src/components/Favorite/Favorite.jsx
@@ -2,33 +2,32 @@ import React from "react";
 // import { useState } from "react";
 import { useSelector } from "react-redux";
 import style from "./Favorite.module.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Card from "../Card/Card";
 
 export const Favorite = (onClose) => {
   const { myFavorites } = useSelector((state) => state);
-  
 
   return (
     <div>
       <div className={style.container}>
-      {
-        myFavorites.map((character)=>  
-
-        <Card
-          key={character.id}
-          name={character.name}
-          species={character.species}
-          gender={character.gender}
-          image={character.image}
-          id={character.id}
-          onClose={() => onClose(character.id)}
-        />
-        
-        )
-      }
-      
-    </div>
+        <div>
+          <h1 className={style.h1Fav}>Favoritos</h1>
+        </div>
+        <div className={style.containerCards}>
+          {myFavorites.map((character) => (
+            <Card
+              key={character.id}
+              name={character.name}
+              species={character.species}
+              gender={character.gender}
+              image={character.image}
+              id={character.id}
+              onClose={() => onClose(character.id)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
