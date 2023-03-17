902c1883d7df178efb9da2d9022bce5b5af5b292
franklyn castro <franklyncastro@hotmail.com>
2023-03-17 16:23:23 -0400

    add async actions.js

--- front/src/redux/action.js
+++ front/src/redux/action.js
@@ -1,19 +1,26 @@
 import { ADD_FAVORITE, DELETE_FAVORITE } from "./action-types";
-import axios from 'axios'
+import axios from "axios";
 
-export const addFavorite = (character) =>{
-
-  axios.post("http://localhost:3001/rickandmorty/fav", character)
-  return {
-    type: ADD_FAVORITE,
-    payload: character,
-  };
-};
-
-export const deleteFavorite = (id) => {
-  axios.delete(`http://localhost:3001/rickandmorty/fav/${id}`)
+export async function addFavorite(character) {
+  try {
+    await axios.post("http://localhost:3001/rickandmorty/fav", character);
     return {
-        type: DELETE_FAVORITE,
-        payload: id
-    }
+      type: ADD_FAVORITE,
+      payload: character,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteFavorite(id) {
+  try {
+    await axios.delete(`http://localhost:3001/rickandmorty/fav/${id}`);
+    return {
+      type: DELETE_FAVORITE,
+      payload: id,
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./reducer";
 import thunkMidleware from "redux-thunk";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //esta linea es para coenctar con la extension del navegador REDUX DEVTOOLS
+//todo => esta linea es para coenctar con la extension del navegador REDUX DEVTOOLS
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
 
 const store = createStore(
     reducer,
-    composeEnhancer(applyMiddleware(thunkMidleware)) //esta linea es para poder hacer peticiones a un server
+    //todo => esta linea es para poder hacer peticiones a un server
+    composeEnhancer(applyMiddleware(thunkMidleware)) 
 );
 
 
