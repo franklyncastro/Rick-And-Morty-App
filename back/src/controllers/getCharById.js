a5f7688247c2df28f065e38f192d8c813abc416f
franklyn castro <franklyncastro@hotmail.com>
2023-03-17 14:59:40 -0400

    add async await en getCharByDetail.js

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -1,29 +1,58 @@
 const axios = require("axios");
-
 const URL = "https://rickandmortyapi.com/api/character";
 
-const getCharById = (req, res) => {
-  const { id } = req.params;
-  axios(`${URL}/${id}`)
-    .then((res) => res.data)
-    .then((data) => {
-      const character = {
-        id: data.id,
-        image: data.image,
-        name: data.name,
-        gender: data.gender,
-        species: data.species,
-      };
-      res.status(200).json(character);
-    })
-    .catch((err) => {
-      res.status(500).json({ err: err.message });
-    });
-};
+//todo => Agregado async await
+async function getCharById(req, res) {
+  try{
+    const { id } = req.params;
+    const response = await axios(`${URL}/${id}`);
+    const data = response.data
+    const character = {
+      id: data.id,
+      image: data.image,
+      name: data.name,
+      gender: data.gender,
+      species: data.species,
+    }
+    res.status(200).json(character);
+  }
+  catch(err){
+    res.status(500).json({ err: err.message });
+  }
+}
+
+
+
+
+
+
+
+
+
 
 module.exports = {
-  getCharById
+  getCharById,
 };
+
+//! proceso peticion api sin Async-Await 
+// const getCharById = (req, res) => {
+//   const { id } = req.params;
+//   axios(`${URL}/${id}`)
+//     .then((res) => res.data)
+//     .then((data) => {
+//       const character = {
+//         id: data.id,
+//         image: data.image,
+//         name: data.name,
+//         gender: data.gender,
+//         species: data.species,
+//       };
+//       res.status(200).json(character);
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ err: err.message });
+//     });
+// };
 
 //! Proceso sin el uso de Express
 // const getCharById = (res, id) => {
