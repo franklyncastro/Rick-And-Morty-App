de12826e072dfea74bb6bd677d12b76143b3bd48
franklyn castro <franklyncastro@hotmail.com>
2023-03-17 15:05:13 -0400

    add sync await details and Id

--- back/src/controllers/getCharByDetail.js
+++ back/src/controllers/getCharByDetail.js
@@ -1,27 +1,50 @@
 const axios = require("axios");
 const URL = "https://rickandmortyapi.com/api/character";
 
-const getCharByDetail = (req, res) => {
-  const { id } = req.params;
-  axios(`${URL}/${id}`)
-    .then((res) => res.data)
-    .then((data) => {
-      const character = {
-        id: data.id,
-        image: data.image,
-        name: data.name,
-        gender: data.gender,
-        species: data.species,
-        status: data.status,
-        origin: data.origin,
-      };
-      res.status(200).json(character);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error.message });
-    });
-};
+//todo => Agregado async await
+async function getCharByDetail(req, res) {
+  try {
+    const { id } = req.params;
+    const response = await axios(`${URL}/${id}`);
+    const data = response.data;
+    const character = {
+      id: data.id,
+      image: data.image,
+      name: data.name,
+      gender: data.gender,
+      species: data.species,
+    };
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
 
 module.exports = {
   getCharByDetail,
 };
+
+
+
+
+
+//! Proceso sin Async-Await
+// const getCharByDetail = (req, res) => {
+//   const { id } = req.params;
+//   axios(`${URL}/${id}`)
+//     .then((res) => res.data)
+//     .then((data) => {
+//       const character = {
+//         id: data.id,
+//         image: data.image,
+//         name: data.name,
+//         gender: data.gender,
+//         species: data.species,
+//         status: data.status,
+//         origin: data.origin,
+//       };
+//       res.status(200).json(character);
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: error.message });
+//     });
+// };
--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -3,38 +3,32 @@ const URL = "https://rickandmortyapi.com/api/character";
 
 //todo => Agregado async await
 async function getCharById(req, res) {
-  try{
+  try {
     const { id } = req.params;
     const response = await axios(`${URL}/${id}`);
-    const data = response.data
+    const data = response.data;
     const character = {
       id: data.id,
       image: data.image,
       name: data.name,
       gender: data.gender,
       species: data.species,
-    }
+    };
     res.status(200).json(character);
-  }
-  catch(err){
+  } catch (err) {
     res.status(500).json({ err: err.message });
   }
 }
-
-
-
-
-
-
-
-
-
 
 module.exports = {
   getCharById,
 };
 
-//! proceso peticion api sin Async-Await 
+
+
+
+
+//! proceso peticion api sin Async-Await
 // const getCharById = (req, res) => {
 //   const { id } = req.params;
 //   axios(`${URL}/${id}`)
