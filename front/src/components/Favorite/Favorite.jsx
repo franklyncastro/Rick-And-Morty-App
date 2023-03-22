55f72fc6f6f439ea36c8d1a2e0e0b424b4c1ac12
franklyn castro <franklyncastro@hotmail.com>
2023-03-22 13:57:04 -0400

    actualizacion css

--- front/src/components/Favorite/Favorite.jsx
+++ front/src/components/Favorite/Favorite.jsx
@@ -2,47 +2,36 @@ import React from "react";
 // import { useState } from "react";
 import { useSelector } from "react-redux";
 import style from "./Favorite.module.css";
-// import { Link } from "react-router-dom";
-import Card from "../Card/Card";
+import { Link } from "react-router-dom";
+import './animaciones.css'
 
 export const Favorite = () => {
   const { myFavorites } = useSelector((state) => state);
-  // console.log(myFavorites);
-  //todo => filtro para evitar que se dupliquen los favoritos
-  //! verificar, aun se duplican los favoritos...  
-  const uniqueFavorites = myFavorites.filter((character, index) => {
-    return myFavorites.indexOf(character) === index;
-  });
 
   return (
-    <div>
-      <div className={style.container}>
-        <div>
-          <h1 className={style.h1Fav}>Favoritos</h1>
-        </div>
-        <div className={style.containerCards}>
-          {/* Con esta  */}
-          {myFavorites.length === 0 ? (
-            <p
-              style={{ color: "violet", marginTop: "300px", fontSize: "24px" }}
-              className="animate__animated animate__backInDown"
-            >
-              ¡Agrega un favorito!
-            </p>
-          ) : (
-            uniqueFavorites.map((character) => (
-              <Card
-                key={character.id}
-                name={character.name}
-                species={character.species}
-                gender={character.gender}
-                image={character.image}
-                id={character.id}
-              />
-            ))
-          )}
-        </div>
-      </div>
+    <div className={style.container}>
+      {myFavorites.map((char) => {
+        return (
+          <div className={style.container}>
+            <div className={style.containerCard}>
+              <div className={style.btnContainer}></div>
+              <span className="animate__animated animate__bounce corazon"> ❤️ </span>
+              <Link to={`/detail/${char.id}`}>
+              <h2 className={style.textInfo}>id: {char.id}</h2>
+                <h2 className={style.textInfo}>Detalle</h2>
+              </Link>
+              <div className={style.containerImg}>
+                <img src={char.image} alt="img not found" />
+              </div>
+              <h2 className={style.nameText}>{char.name}</h2>
+              <div className={style.containerText}>
+                <h2 className={style.textInfo}>{char.species}</h2>
+                <h2 className={style.textInfo}>{char.gender}</h2>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
